171ac72ad31d329a2d027f0102f6b7c28529bcc3
Isaque Dutra <dutra@mit.edu>
2015-04-15 22:52:21 -0400

    Added puts function in boot.asm.

--- Makefile
+++ Makefile
@@ -3,13 +3,20 @@ ARMGNU = arm-none-eabi
 ASFLAGS =  -mcpu=cortex-a8
 ALLFLAGS = --warn --fatal-warnings
 CFLAGS = -Wall -Werror -O2 -nostdlib -nostartfiles -ffreestanding -mcpu=cortex-a8 -mno-unaligned-access -marm -fno-builtin -fno-stack-protector
-LFLAGS = -m armelf -nostdlib -nostdinc -nodefaultlibs -nostartfiles -T linker.ld
+LDFLAGS = -m armelf -nostdlib -nostdinc -nodefaultlibs -nostartfiles -T linker.ld
 OCFLAGS = --target elf32-littlearm --set-section-flags .bss=contents,alloc,load
+OBJDIR = obj
 
+.PHONY: clean
 
-boot.img: boot.asm
-	$(ARMGNU)-as $(ASFLAGS) -c boot.asm -o boot.o
-	$(ARMGNU)-ld -dn $(LDFLAGS) boot.o -o boot.elf
-	$(ARMGNU)-objdump -D boot.elf > boot.list
-	$(ARMGNU)-objcopy boot.elf -O srec boot.srec
-	$(ARMGNU)-objcopy $(OCFLAGS) -O binary boot.elf boot.img
+all: boot.bin
+
+boot.bin: boot.asm
+	$(ARMGNU)-as $(ASFLAGS) -c boot.asm -o $(OBJDIR)/boot.o
+	$(ARMGNU)-ld -dn $(LDFLAGS) $(OBJDIR)/boot.o -o $(OBJDIR)/boot.elf
+	$(ARMGNU)-objdump -D $(OBJDIR)/boot.elf > $(OBJDIR)/boot.list
+	$(ARMGNU)-objcopy $(OBJDIR)/boot.elf -O srec $(OBJDIR)/boot.srec
+	$(ARMGNU)-objcopy $(OCFLAGS) -O binary $(OBJDIR)/boot.elf $(OBJDIR)/boot.bin
+
+clean:
+	rm -rf $(OBJDIR)/*
--- boot.asm
+++ boot.asm
@@ -5,47 +5,52 @@
 	.equ UART0.LSR_UART,    0x44E09014
 	.equ UART0.SSR,    			 0x44E09044
 
+.text
 	.global _start
-
-	# ARM mode
 	.arm
+	.align
 
 _start:
-	ldr r0, =UART0.BASE
-	mov r1, #'!'
-	strb r1, [r0]
-
+	ldr r1, =str_hello
+	bl _puts
 _loop:
+	b _loop
+
+###############################################
+# PUTS
+# R1: Address of null-terminated string to be printed
+###############################################
+_puts:
+	push {r0, r1, r2, lr}
+	mov r0, r1
+_puts_loop:
+	ldrb r1, [r0], #1
+	cmp r1, #0
+	beq _puts_end
+	bl _putc
+	b _puts_loop
+_puts_end:
+	pop {r0, r1, r2, pc}
+
+###############################################
+# PUTC
+# R1: Character to be printed
+###############################################
+_putc:
+	push {r0, r1, r2, lr}
 	ldr r0, =UART0.SSR
 	ldrb r2, [r0]
 	# Bit 0 is set when TX FIFO is FULL
 	and r2, r2, #0x01
 	cmp r2, #0x01
 	# Branch when Z is set, Bit 1 set
-	beq _loop
-
-	# Check if data in RX FIFO
-	# First need to clear LCR[7] to access LSR_UART
-	ldr r0, =UART0.LCR
-	ldrb r1, [r0]
-	and r1, r1, #0xFFFFFF7F
-	strb r1, [r0]
-
-	# Then read LSR_UART
-	ldr r0, =UART0.LSR_UART
-	ldrb r1, [r0]
-	and r1, r1, #0x01
-	# LSR_UART[0] is high when data is available in RX FIFO
-	cmp r1, #0x01
-	# Branch if LSR_UART[0] is low
-	bne _loop
+	beq _putc
 
 	ldr r0, =UART0.BASE
-	# Need to read RHR to pop FIFO
-	ldrb r1, [r0]
-	#mov r1, #'e'
 	strb r1, [r0]
-	b _loop
 
-_hang:
-	b _hang
+	pop {r0, r1, r2, pc}
+
+	.data
+str_hello:
+	.asciz "Hello World!\r\n"
