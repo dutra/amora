f8049807e022328e7189e5d29957789a1f8a3b70
Isaque Dutra <dutra@mit.edu>
2015-04-19 14:23:38 -0400

    Added _enable_cache function to enable Instruction Cache, Data Cache, and Branch Prediction.

--- include/armv7a.h
+++ include/armv7a.h
@@ -3,4 +3,9 @@
 #define UART0_BASE           0x44E09000
 #define UART0_LCR   	      0x44E0900C
 #define UART0_LSR_UART   0x44E09014
-#define UART0_SSR  			 0x44E09044
+#define UART0_SSR  			  0x44E09044
+
+/* Control Register C1 Co-processor */
+#define C1CTL_I                   0x00001000
+#define C1CTL_Z                  0x00000800
+#define C1CTL_C                  0x00000004
